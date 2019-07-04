a3fa5e199788fe55cafbc1c81c5838f93cc51958
Topi Mattias Luukkanen <topi.m.luukkanen@student.jyu.fi>
2019-07-04 16:06:43 +0300

    tehtävä 1.3

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -1,15 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// Refaktoroi sovelluksen koodi siten, että se koostuu kolmesta uudesta komponentista: Header, Content ja Total. 
-
 const Header = (props) => {
+    console.log("Header props" + props)
+    console.log(props)
+
     return (
         <h1>{props.course}</h1>
     )
 }
 
 const Content = (props) => {
+    console.log("Content props" + props)
+    console.log(props)
+
     return (
         <div>
             <Part part={props.part1} exercise={props.exercises1} />
@@ -20,6 +24,9 @@ const Content = (props) => {
 }
 
 const Total = (props) => {
+    console.log("Total props" + props)
+    console.log(props)
+
     return (
         <p>
             Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
@@ -28,6 +35,10 @@ const Total = (props) => {
 }
 
 const Part = (props) => {
+    console.log("Part props")
+    console.log(props)
+
+
     return (
         <p>
             {props.part} {props.exercise}
@@ -37,25 +48,27 @@ const Part = (props) => {
 
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
+    const part1 = {
+      name: 'Fundamentals of React',
+      exercises: 10
+    }
+    const part2 = {
+      name: 'Using props to pass data',
+      exercises: 7
+    }
+    const part3 = {
+      name: 'State of a component',
+      exercises: 14
+    }
 
     return (
         <div>
             <Header course={course}/>
-            <Content part1={part1} part2={part2} part3={part3}
-                    exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
-            <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+            <Content part1={part1.name} part2={part2.name} part3={part3.name}
+                    exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
+            <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
         </div>
     )
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
