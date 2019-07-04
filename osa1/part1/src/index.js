80456fc233da4f2439474ff44df99685151efe0f
Topi Mattias Luukkanen <topi.m.luukkanen@student.jyu.fi>
2019-07-04 14:28:20 +0300

    tehtävä 1.2

--- osa1/part1/src/index.js
+++ osa1/part1/src/index.js
@@ -11,25 +11,27 @@ const Header = (props) => {
 
 const Content = (props) => {
     return (
-        <>
-            <p>
-                {props.osa1} {props.exercises1}
-            </p>
-            <p>
-                {props.part2} {props.exercises2}
-            </p>
-            <p>
-                {props.part3} {props.exercises3}
-            </p>
-        </>
+        <div>
+            <Part part={props.part1} exercise={props.exercises1} />
+            <Part part={props.part2} exercise={props.exercises2} />
+            <Part part={props.part3} exercise={props.exercises3} />
+        </div>
     )
 }
 
 const Total = (props) => {
     return (
-        <>
-           <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p> 
-        </>
+        <p>
+            Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}
+        </p> 
+    )
+}
+
+const Part = (props) => {
+    return (
+        <p>
+            {props.part} {props.exercise}
+        </p>
     )
 }
 
