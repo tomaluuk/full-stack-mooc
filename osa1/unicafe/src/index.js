a1b21ea3aecf70a7066ed1b905c88dec87247917
Topi Mattias Luukkanen <topi.m.luukkanen@student.jyu.fi>
2019-07-12 00:50:26 +0300

    ´1.7 unicafe step2

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -13,6 +13,9 @@ const Stats = ({good, neutral, bad}) => {
       <p>good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>all {good + neutral + bad}</p>
+      <p>average {(good - bad) / (good + neutral + bad)}</p>
+      <p>positive {good / (good + neutral + bad) * 100}%</p>
     </div>
   )
 }
