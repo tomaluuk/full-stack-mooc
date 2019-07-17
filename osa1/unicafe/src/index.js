f0677b6813af29c5e7a8488731100b4ea7b12716
Topi Mattias Luukkanen <topi.m.luukkanen@student.jyu.fi>
2019-07-17 21:36:22 +0300

    1.8 & 1.9 unicafe step3 & step4: ehdollinen renderöinti

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -8,16 +8,18 @@ const Button = ({text, handleClick}) => {
 }
 
 const Stats = ({good, neutral, bad}) => {
-  return (
-    <div>
-      <p>good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>all {good + neutral + bad}</p>
-      <p>average {(good - bad) / (good + neutral + bad)}</p>
-      <p>positive {good / (good + neutral + bad) * 100}%</p>
-    </div>
-  )
+  if (good !== 0 || neutral !== 0 || bad !== 0) {
+    return (
+      <div>
+        <p>good {good}</p>
+        <p>neutral {neutral}</p>
+        <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>average {(good - bad) / (good + neutral + bad)}</p>
+        <p>positive {good / (good + neutral + bad) * 100}%</p>
+      </div>
+    )
+  } else return <p>Give feedback to show statistics</p>;
 }
 
 const App = (props) => {
