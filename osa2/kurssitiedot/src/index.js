73b23b29e7169f2e46b3a9fb15892c1363f67082
Topi Mattias Luukkanen <topi.m.luukkanen@student.jyu.fi>
2019-07-24 14:28:45 +0300

    Uuden projektin pohjan commit

---
+++ osa2/kurssitiedot/src/index.js
@@ -0,0 +1,73 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+    console.log('Header props', props)
+
+    return (
+        <h1>{props.course}</h1>
+    )
+}
+
+const Content = (props) => {
+    console.log('Content props', props)
+
+    return (
+        <div>
+            <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
+            <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
+            <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
+        </div>
+    )
+}
+
+const Total = (props) => {
+    console.log('Total props', props)
+
+    return (
+        <p>
+            Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+        </p> 
+    )
+}
+
+const Part = (props) => {
+    console.log('Part props', props)
+
+    return (
+        <p>
+            {props.part} {props.exercise}
+        </p>
+    )
+}
+
+const App = () => {
+    const course = {
+        name: 'Half Stack application development',
+        parts: [
+          {
+            name: 'Fundamentals of React',
+            exercises: 10
+          },
+          {
+            name: 'Using props to pass data',
+            exercises: 7
+          },
+          {
+            name: 'State of a component',
+            exercises: 14
+          }
+        ]
+      }
+  
+
+    return (
+        <div>
+            <Header course={course.name}/>
+            <Content parts={course.parts} />
+            <Total parts={course.parts} />
+        </div>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
