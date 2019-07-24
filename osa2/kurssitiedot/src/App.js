97615da781cfc91fcecfecbea36d396a14851146
Topi Mattias Luukkanen <topi.m.luukkanen@student.jyu.fi>
2019-07-24 15:53:18 +0300

    2.1 kurssitiedot step6

--- osa2/kurssitiedot/src/App.js
+++ osa2/kurssitiedot/src/App.js
@@ -1,26 +1,83 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const Course = ({ course }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header course={course} />
+      <Content course={course} />
     </div>
   );
+}
+
+
+const Header = ({ course }) => {
+  console.log('Header props', course)
+
+  return (
+      <h1>{course.name}</h1>
+  )
+}
+
+const Content = ({ course }) => {
+  console.log('Content props', course)
+
+  return (
+      <div>
+          <Part part={course.parts[0].name} exercise={course.parts[0].exercises} />
+          <Part part={course.parts[1].name} exercise={course.parts[1].exercises} />
+          <Part part={course.parts[2].name} exercise={course.parts[2].exercises} />
+      </div>
+  )
+}
+
+const Part = (props) => {
+  console.log('Part props', props)
+
+  return (
+    <p>
+      {props.part}, {props.exercise} excercises
+    </p>
+  )
+}
+/* 
+const Total = (props) => {
+  console.log('Total props', props)
+
+  return (
+      <p>
+          Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
+      </p> 
+  )
+}
+ */
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
 }
 
 export default App;
--- osa2/kurssitiedot/src/index.js
+++ osa2/kurssitiedot/src/index.js
@@ -1,73 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
 
-const Header = (props) => {
-    console.log('Header props', props)
+ReactDOM.render(<App />, document.getElementById('root'));
 
-    return (
-        <h1>{props.course}</h1>
-    )
-}
-
-const Content = (props) => {
-    console.log('Content props', props)
-
-    return (
-        <div>
-            <Part part={props.parts[0].name} exercise={props.parts[0].exercises} />
-            <Part part={props.parts[1].name} exercise={props.parts[1].exercises} />
-            <Part part={props.parts[2].name} exercise={props.parts[2].exercises} />
-        </div>
-    )
-}
-
-const Total = (props) => {
-    console.log('Total props', props)
-
-    return (
-        <p>
-            Number of exercises {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}
-        </p> 
-    )
-}
-
-const Part = (props) => {
-    console.log('Part props', props)
-
-    return (
-        <p>
-            {props.part} {props.exercise}
-        </p>
-    )
-}
-
-const App = () => {
-    const course = {
-        name: 'Half Stack application development',
-        parts: [
-          {
-            name: 'Fundamentals of React',
-            exercises: 10
-          },
-          {
-            name: 'Using props to pass data',
-            exercises: 7
-          },
-          {
-            name: 'State of a component',
-            exercises: 14
-          }
-        ]
-      }
-  
-
-    return (
-        <div>
-            <Header course={course.name}/>
-            <Content parts={course.parts} />
-            <Total parts={course.parts} />
-        </div>
-    )
-}
-
-ReactDOM.render(<App />, document.getElementById('root'))
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
