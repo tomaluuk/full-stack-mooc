4324505a676a7ffbab9d29682363c3aea437be22
Topi Mattias Luukkanen <topi.m.luukkanen@student.jyu.fi>
2019-08-02 17:21:04 +0300

    2.9* puhelinluettelo step4: hakumahdollisuus, case sensitive

--- osa2/puhelinluettelo/src/App.js
+++ osa2/puhelinluettelo/src/App.js
@@ -2,15 +2,16 @@ import React, { useState } from 'react'
 import Name from './Name';
 
 const App = () => {
-  const [ persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas',
-      number: '040 654 9871',
-      id: 'Arto Hellas' 
-    }
+  const [ contacts, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040 654 9871', id: 'Arto Hellas' },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 'Ada Lovelace' },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 'Dan Abramov' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 'Mary Poppendieck' }
   ]) 
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ searchQuery, setSearchQuery ] = useState('')
+  const [ showAll, setShowAll ] = useState(true)
 
   const addContact = (event) => {
     event.preventDefault()
@@ -21,12 +22,12 @@ const App = () => {
       id: newName
     }
     
-    const isDuplicate = persons.find( (nameInList) => {      
+    const isDuplicate = contacts.find( (nameInList) => {      
       return nameInList.id === newListObject.id
     }) 
     
     if(!isDuplicate) {
-      setPersons(persons.concat(newListObject))
+      setPersons(contacts.concat(newListObject))
       
       console.log('nimi lisätty luetteloon')
       console.log('form submitted', event.target)     
@@ -34,6 +35,10 @@ const App = () => {
     }
     else alert(`${newName} on jo puhelinluettelossa`)
   }
+
+  const shownContacts = showAll 
+    ? contacts 
+    : contacts.filter(contact => contact.name.search(searchQuery) >= 0)
 
   const handleName = (event) => {
     //console.log(event.target.value)
@@ -45,7 +50,21 @@ const App = () => {
     setNewNumber(event.target.value)
   }
 
-  const rows = () => persons.map( person => 
+  const handleSearchQuery = (event) => {
+    console.log(event.target.value)
+    setSearchQuery(event.target.value)
+  }
+
+  const handleSearch = (event) => {
+    event.preventDefault()
+    console.log(searchQuery)
+    console.log(searchQuery === '')
+    
+    setShowAll(searchQuery === '')
+
+  }
+
+  const rows = () => shownContacts.map( person => 
     <Name key={person.id} 
           name={person.name} 
           number={person.number} />
@@ -54,6 +73,11 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
+      <form onSubmit={handleSearch}>
+        <div><input onChange={handleSearchQuery} /></div>
+        <div><button type="submit">search</button></div>
+      </form>
+      <h2>Add a new contact</h2>
       <form onSubmit={addContact}>
         <div> name: <input onChange={handleName} /></div>
         <div>number: <input onChange={handleNumber} /></div>
